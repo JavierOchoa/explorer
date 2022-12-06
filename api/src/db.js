72c46bbd66f0c99b030ff6c54555bf942f2732db
Javier Ochoa <jochoaalv@gmail.com>
2022-12-06 15:17:08 -0500

    Update DB variables

--- api/src/db.js
+++ api/src/db.js
@@ -3,18 +3,18 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
+  PGUSER, PGPASSWORD, PGHOST, PGDATABASE, PGHOST
 } = process.env;
 
 let sequelize =
     process.env.NODE_ENV === "production"
         ? new Sequelize({
-          database: DB_NAME,
+          database: PGDATABASE,
           dialect: "postgres",
-          host: DB_HOST,
-          port: 5432,
-          username: DB_USER,
-          password: DB_PASSWORD,
+          host: PGHOST,
+          port: PGHOST,
+          username: PGUSER,
+          password: PGPASSWORD,
           pool: {
             max: 3,
             min: 1,
@@ -30,7 +30,7 @@ let sequelize =
           ssl: true,
         })
         : new Sequelize(
-            `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+            `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGHOST}/${PGDATABASE}`,
             { logging: false, native: false }
         );
 
